16aa95acdcb05e29925f6dddb61cbd59f9e8c5ff
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-25 21:25:58 +0300

    Make it less

--- _printf.c
+++ _printf.c
@@ -11,33 +11,33 @@ va_list arguments;
 const char *p;
 int num = 0;
 va_start(arguments, format);
-if (format == NULL || (format[0] == % && format[1] == 0))
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 return (-1);
 for (p = format; *p; p++)
 {
-if (*p == % &&  *p + 1 == %)
+if (*p == '%' &&  *p + 1 == '%')
 {
 _putchar(*p), num++;
 continue;
 }
-else if (*p == %  &&  *p + 1 != %)
+else if (*p == '%'  &&  *p + 1 != '%')
 {
 switch (*++p)
 {
-case s:
+case 's':
 num += fun_string(arguments);
 break;
-case c:
+case 'c':
 num += fun_character(arguments);
 break;
-case %:
-_putchar(%), num++;
+case '%':
+_putchar('%'), num++;
 break;
-case i: case d:
+case 'i': case 'd':
 num += fun_integer(arguments);
 break;
 default:
-_putchar(%), _putchar(*p), num += 2;
+_putchar('%'), _putchar(*p), num += 2;
 }
 }
 else
