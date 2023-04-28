0792d7215afa5d5009b085ccb67bf73c55d1c310
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-28 11:27:04 -0700

    conversion specifiers: b

---
+++ fun_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* fun_bin - unsigned int argument is converted to binary
+* @arg: is a table of contact
+* Return: int
+*/
+
+int fun_bin(va_list arg)
+{
+unsigned int x;
+int i, n;
+ar[100];
+
+x = va_arg(arg, int);
+
+if (x == 0)
+{
+putchar(0);
+return (1);
+}
+i = 0;
+while (x > 0)
+{
+ar[i] = x % 2;
+x /= 2;
+i++;
+}
+
+for (n = i - 1; n >= 0; n--)
+putchar(ar[n] + 0);
+return (i);
+}
