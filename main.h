b2dca0e799f0cd6c2bcdc03d93d8d0b1525e97a1
alidrisy <125469989+alidrisy@users.noreply.github.com>
2023-04-28 22:21:12 +0300

    Add the conversion specifiers: u o x X

--- main.h
+++ main.h
@@ -14,5 +14,9 @@ int fun_character(va_list c);
 int fun_string(va_list s);
 int _strlen(char *s);
 int fun_bin(va_list arg);
+int fun_uns(va_list args);
+int fun_hex(va_list arg);
+int fun_Hex(va_list arg);
+int fun_oct(va_list arg);
 
 #endif
